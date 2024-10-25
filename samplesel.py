b9f0982c8b917ff27d2d4eba2f0d11115ec857d4
JOY-QA <okochejoy8@gmail.com>
2024-10-25 23:59:00 +0100

    Add files via upload

---
+++ samplesel.py
@@ -0,0 +1,6 @@
+
+from selenium import webdriver
+driver = webdriver.chrome("C:\Program Files\Google\Chrome\Application")
+driver.get ("https://www.google.com")
+driver.find_element_by_name("q").send_keys("selenium")
+driver.find_element_by_name("btnl").click()
